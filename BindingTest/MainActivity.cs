3775d03ebc64cc3061b3c99f831eb49999769dba
Prashant Cholachagudda <pvc@outlook.com>
2016-01-09 18:57:07 +0530

    Initial commit

---
+++ BindingTest/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Consumeapi;
+
+namespace BindingTest
+{
+	[Activity (Label = "BindingTest", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				int value = ConsumeAPI.Function1();
+				string stringValue = ConsumeAPI.Function11(); 
+
+				System.Console.WriteLine ("Function1 - {0}, Function 11 - {1}", value, stringValue);
+			};
+		}
+	}
+}
+
+
